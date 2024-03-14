e579b55d0abb4f1fab8a2a9fc7eb621a862dd866
REZZOUQ <mouad-rezzouq@hotmail.com>
2024-03-14 20:35:33 +0100

    initialisation du repos

---
+++ src/main/java/org/atelier/tenisplayer/MouadRezzouqAtelierApplication.java
@@ -0,0 +1,13 @@
+package org.atelier.tenisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MouadRezzouqAtelierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MouadRezzouqAtelierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/atelier/tenisplayer/MouadRezzouqAtelierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atelier.tenisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MouadRezzouqAtelierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
